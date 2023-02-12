ecb3307c4ee2e23500e83611773c71f6fca8cddd
Supercoder2311 <84426763+Supercoder2311@users.noreply.github.com>
2023-02-12 11:31:48 -0800

    Add files via upload

--- main.js
+++ main.js
@@ -21,6 +21,9 @@ function modelLoaded(){
 function gotPoses(results){
     if(results.length > 0){
         console.log(results);
+        scoreLeftWrist = results[0].pose.keypoints[9].score;
+        console.log(" scoreLeftWrist = " + scoreLeftWrist);
+
        leftWristX= results[0].pose.leftWrist.x;
        leftWristY= results[0].pose.leftWrist.y;
        console.log(" leftWristX = "+leftWristX+" leftWristY = "+leftWristY);
@@ -33,6 +36,17 @@ function gotPoses(results){
 
 function draw(){
     image(video, 0, 0, 600, 500);
+
+    fill("#1cff08");
+    stroke("#1cff08");
+    if(scoreLeftWrist > 0.2){
+        circle(leftWristX, leftWristY, 20);
+        InNumberleftWristY = Number(leftWristY);
+        remove_decimals = floor(InNumberleftWristY);
+        volume = remove_decimals/500;
+        document.getElementById("volume").innerHTML= " Volume = " + volume;
+        song.setVolume(volume);
+    }
 }
 
 function playSound(){
